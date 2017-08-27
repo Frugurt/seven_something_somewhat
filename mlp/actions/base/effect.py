37102387bbf5f1b5e6903578fa08bbc969dc0888
Ecialo <ecialo@yandex.ru>
2017-08-27 14:49:18 +0300

    Чистка кода

--- mlp/actions/base/effect.py
+++ mlp/actions/base/effect.py
@@ -37,13 +37,9 @@ class AbstractEffect(metaclass=EffectMeta):
 
     def __init__(self, **kwargs):
         self.is_canceled = False
-        print(self.name, kwargs)
         self.extra_tags = kwargs.get('extra_tags', [])
-        print("\n\nEXXTRA TAGSSS", self.name, self.tags)
-        # self
 
     def log(self, source):
-        # source.action_log.append(self.info_message)
         logger.debug(
             "Effect {} with context".format(self.name)
         )
@@ -55,7 +51,6 @@ class AbstractEffect(metaclass=EffectMeta):
     @contextmanager
     def configure(self, context):
         context_values = dotdict()
-        # print("CONFIGURE", vars(self))
         for k, v in vars(self).items():
             if isinstance(v, Property):
                 context_values[k] = v.get(context)
@@ -69,51 +64,32 @@ class AbstractEffect(metaclass=EffectMeta):
     def cancel(self):
         self.is_canceled = True
 
-    # def __copy__(self):
-    #     pass
-
 
 class UnitEffect(AbstractEffect):
 
-    # take_event_name = "on_take_" + convert(__name__)
-    # after_event_name = "after_take_" + convert(__name__)
-
     def __init__(self, **kwargs):
-        # self.owner = owner
-        # self.source = source
         self.info_message = self.info_message
         super().__init__(**kwargs)
-        # self.take_event_name = "on_take_" + convert(self.name or self.__class__.__name__)
-        # self.
 
     def _apply(self, target, context):
         source = context['source']
-        # self.configure(context)
         self.log(source)
 
     def apply(self, cells, context):
         if context['owner'].state is PLANNING and "plan" not in self.tags:
-            # print("FAIL!!!!!!!!!!!!!!!!!!!!!!")
             return
         if not isinstance(cells, Iterable):
             cells = [cells]
         for cell in cells:
             if cell.object is not None:
-                # unit = cell.object
                 effect_context = context.copy()
                 effect_context['target'] = cell.object
                 effect = self.copy()
-                # print("EFFECT EVENT", self.take_event_name)
                 cell.object.launch_triggers(self.tags, effect, effect_context)
                 if not effect.is_canceled:
                     effect._apply(cell.object, effect_context)
-                    # cell.object.launch_triggers(self.after_event_name, self, source)
-        # source_action.owner.action_log.append(self.info_message)
 
     def copy(self):
-        # params = vars(self)
-        # params.pop("is_canceled", None)
-        # return self.__class__(**params)
         return self.__class__(**vars(self))
 
 
@@ -121,12 +97,10 @@ class CellEffect(AbstractEffect):
 
     def _apply(self, cell, context):
         source = context['source']
-        # self.configure(context)
         self.log(source)
 
     def apply(self, cells, context):
         if context['owner'].state is PLANNING and "plan" not in self.tags:
-            # print("FAIL!!!!!!!!!!!!!!!!!!!!!!")
             return
         if not isinstance(cells, Iterable):
             cells = [cells]
@@ -163,7 +137,6 @@ class CustomUnitEffect(UnitEffect):
     effects = []
 
     def __init__(self, **kwargs):
-        # assert set(self.params) == set(kwargs.keys())
         super().__init__(**kwargs)
         for k, v in kwargs.items():
             setattr(self, k, v)
@@ -180,34 +153,15 @@ class CustomUnitEffect(UnitEffect):
                 effect._apply(target, context)     # TODO перепроектировать это
 
 
-# def effect_constructor(loader, node):
-#     e_s = {}
-#     for key_node, value_node in node.value:
-#         # print("\n\nTAG", value_node.tag)
-#         if isinstance(value_node, SequenceNode) and value_node.tag == "tag:yaml.org,2002:seq":
-#             value = loader.construct_sequence(value_node)
-#         else:
-#             value = loader.construct_object(value_node)
-#         e_s[key_node.value] = value
-#     # e_s = loader.construct_mapping(node)
-#     name = e_s.pop("name")
-#     effect = EFFECTS[name](**e_s)
-#     return effect
-#
-
 def effect_constructor(loader, node):
     e_s = {}
     for key_node, value_node in node.value:
-        # print("\n\nTAG", value_node.tag)
         if isinstance(value_node, SequenceNode) and value_node.tag == "tag:yaml.org,2002:seq":
             value = loader.construct_sequence(value_node)
         else:
             value = loader.construct_object(value_node)
         e_s[key_node.value] = value
-    # e_s = loader.construct_mapping(node)
     name = e_s.pop("name")
-    print(e_s)
-    # effect = EFFECTS[name](**e_s)
     return Reference(name, e_s, EFFECTS)
 
 
